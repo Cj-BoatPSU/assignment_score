b021c6c58f5885aa99354b17225cf648af47e1f1
Chutiwat Jiantiw <6010110088@R301-mini10.local>
2020-08-26 17:48:34 +0700

    In class commit

---
+++ lib/config/routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const home ='/';
+  static const show_score = '/show-score';
+  static const edit_score = '/edit-score';
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:js';
+
+import 'package:assigement_1/config/routes.dart';
+import 'package:assigement_1/presentation/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Assignment',
+      routes: {
+        AppRoutes.home: (context) => Homepage()
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), 
+    );
+  }
+}
